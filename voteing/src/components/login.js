9e6ddea89f0328a90ba64be209380856fcf08ec1
413-AYISHA <ayishaafrose413@gmail.com>
2024-08-06 13:30:02 +0530

    login finish

--- voteing/src/components/login.js
+++ voteing/src/components/login.js
@@ -1,35 +1,38 @@
 // src/Login.js
 import React from 'react';
-import '../css/App.css'; 
+// import '../css/login.css'; 
 
 
 const Login = () => {
   return (
-    
-    <section className="bg-black-50 min-h-screen flex items-center justify-center">
-      <div className="bg-[#f1e9e9dd] flex rounded-2xl shadow-lg max-w-3xl p-4">
+  
+    <section className="bg-grey-500 login-bg min-h-screen flex items-center justify-center">
+      <div className="bg-[#fffcfdad]  flex rounded-2xl shadow-lg max-w-3xl p-4">
         <div className="sm:w-1/2 px-16">
-          <h2 className="font-bold text-2xl text-[#4527a5] text-center">Login</h2>
-          <p class="text-sm mt-7 text-[#6c57b1] text-opacity-70 text-center">If you already a member, easily log in</p>
+          <h2 className="font-bold text-2xl text-blue-500 text-center">Login</h2>
+          <p class="text-sm mt-7 text-black text-opacity-70 text-center">If you already a member, easily log in</p>
           <form className="flex flex-col gap-4" action="">
-            <input className="p-2 mt-8 rounded-xl border" type="text" name="email" placeholder="Your email" />
-            <div className="relative">
-              <input className="p-2 mt-8 rounded-xl border w-full" type="password" name="password" placeholder="Your password" />
+            <input className="p-2 mt-8 rounded-xl border focus:outline-none" type="text" name="email" placeholder="Your email" />
+            <div className="">
+              <input className="p-2 mt-1 rounded-xl border w-full focus:outline-none" type="password" name="password" placeholder="Your password" />
             </div>
             <button className="Login-button rounded-xl text-white py-2">Login</button>
           </form>
           <div className="mt-3 text-xs flex justify-between items-center">
             <p>
-              <a href="#">Forgot Password?</a>
+              <a href="#" className=''>Forgot Password?</a>
             </p>
             
-            <button className="py-2 px-5 bg-white border rounded-xl">Register</button>
+            <button className="py-2 px-5 text-white  Login-button rounded-xl">Register</button>
           </div>
         </div>
 
-        <img src={Login} alt=" "/>
-        <img src={require('../assets/lock.jpeg')}alt=" "  width="350"
-              height="350"/>
+        
+        {/* <img className='sm:w-2/3 md:w-1/2' src={require('../assets/lock.jpeg')} alt="LOGIN IMAGE "  width="350"
+              height="250"/> */}
+              <div className="md:w-1/2 flex img_hid items-center justify-center">
+      <img className="w-full  rounded-xl" src={require('../assets/girl.png')} alt="LOGIN IMAGE" />
+    </div>
       
       </div>
     </section>
