754c1bcdcc69c782c158f2dd7a481b4d2da9ed60
413-AYISHA <ayishaafrose413@gmail.com>
2024-08-06 15:29:24 +0530

    login finish1

--- voteing/src/App.js
+++ voteing/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './css/login.css';
 
+
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Votepage from './pages/Votepage';
--- voteing/src/components/login.js
+++ voteing/src/components/login.js
@@ -1,39 +1,67 @@
-// src/Login.js
-import React from 'react';
-// import '../css/login.css'; 
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
+import login_img from '../assets/girl.png';
 
+const PasswordInput = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <div className="relative">
+      <FontAwesomeIcon
+        icon={isPasswordVisible ? faUnlock : faLock}
+        className="absolute right-3 top-2/4 transform -translate-y-1/2 text-blue-400 cursor-pointer"
+        onClick={togglePasswordVisibility}
+      />
+      <input
+        className="p-2 pl-10 mt-1 rounded-xl border focus:outline-none w-full"
+        type={isPasswordVisible ? "text" : "password"}
+        name="password"
+        placeholder="Your password"
+      />
+    </div>
+  );
+};
 
 const Login = () => {
   return (
-  
     <section className="bg-grey-500 login-bg min-h-screen flex items-center justify-center">
-      <div className="bg-[#fffcfdad]  flex rounded-2xl shadow-lg max-w-3xl p-4">
+      <div className="bg-[#fffcfd80] flex rounded-2xl shadow-lg max-w-3xl p-4">
         <div className="sm:w-1/2 px-16">
           <h2 className="font-bold text-2xl text-blue-500 text-center">Login</h2>
-          <p class="text-sm mt-7 text-black text-opacity-70 text-center">If you already a member, easily log in</p>
+          <p className="text-sm mt-7 text-black text-opacity-70 text-center">
+            If you already a member, easily log in
+          </p>
           <form className="flex flex-col gap-4" action="">
-            <input className="p-2 mt-8 rounded-xl border focus:outline-none" type="text" name="email" placeholder="Your email" />
-            <div className="">
-              <input className="p-2 mt-1 rounded-xl border w-full focus:outline-none" type="password" name="password" placeholder="Your password" />
+            <div className="relative">
+              <FontAwesomeIcon icon={faEnvelope} className="absolute right-3 top-3/4 transform -translate-y-1/2 text-blue-400" />
+              <input
+                className="p-2 pl-10 mt-8 rounded-xl border focus:outline-none w-full"
+                type="text"
+                name="email"
+                placeholder="Your email"
+              />
             </div>
+            <PasswordInput />
             <button className="Login-button rounded-xl text-white py-2">Login</button>
           </form>
           <div className="mt-3 text-xs flex justify-between items-center">
             <p>
-              <a href="#" className=''>Forgot Password?</a>
+              
+                Forgot Password?
+              
             </p>
-            
-            <button className="py-2 px-5 text-white  Login-button rounded-xl">Register</button>
+            <button className="py-2 px-5 text-white Login-button rounded-xl">Register</button>
           </div>
         </div>
-
         
-        {/* <img className='sm:w-2/3 md:w-1/2' src={require('../assets/lock.jpeg')} alt="LOGIN IMAGE "  width="350"
-              height="250"/> */}
-              <div className="md:w-1/2 flex img_hid items-center justify-center">
-      <img className="w-full  rounded-xl" src={require('../assets/girl.png')} alt="LOGIN IMAGE" />
-    </div>
-      
+        <div className="md:w-1/2 flex img_hid items-center justify-center">
+          <img className="w-full rounded-xl" src={login_img} alt='loginimage' />
+        </div>
       </div>
     </section>
   );
