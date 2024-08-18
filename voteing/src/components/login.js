eab99ddfa603e4ad006e8bfbee0de8f5677d9a7a
413-AYISHA <ayishaafrose413@gmail.com>
2024-08-18 22:55:46 +0530

    not yet finish

--- voteing/src/components/login.js
+++ voteing/src/components/login.js
@@ -3,12 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock, faUnlock } from '@fortawesome/free-solid-svg-icons';
 import login_img from '../assets/girl.png';
 
+
 const PasswordInput = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
+  
+  <button className="Login-button glow-effect rounded-xl text-black py-2">Login</button>
 
   return (
     <div className="relative">
@@ -18,10 +21,10 @@ const PasswordInput = () => {
         onClick={togglePasswordVisibility}
       />
       <input
-        className="p-2 pl-10 mt-1 rounded-xl border focus:outline-none w-full"
+        className="p-2 pl-1 mt-1 rounded-xl border focus:outline-none w-full"
         type={isPasswordVisible ? "text" : "password"}
         name="password"
-        placeholder="Your password"
+        placeholder="Enter password"
       />
     </div>
   );
@@ -29,6 +32,7 @@ const PasswordInput = () => {
 
 const Login = () => {
   return (
+    
     <section className="bg-grey-500 login-bg min-h-screen flex items-center justify-center">
       <div className="bg-[#fffcfd80] flex rounded-2xl shadow-lg max-w-3xl p-4">
         <div className="sm:w-1/2 px-16">
@@ -36,28 +40,32 @@ const Login = () => {
           <p className="text-sm mt-7 text-black text-opacity-70 text-center">
             If you already a member, easily log in
           </p>
+        
+
           <form className="flex flex-col gap-4" action="">
             <div className="relative">
-              <FontAwesomeIcon icon={faEnvelope} className="absolute right-3 top-3/4 transform -translate-y-1/2 text-blue-400" />
+              <FontAwesomeIcon icon={faEnvelope} className="absolute right-3 top-2/3 transform -translate-y-1/2 text-blue-400" />
               <input
-                className="p-2 pl-10 mt-8 rounded-xl border focus:outline-none w-full"
+                className="p-2 pl-1 mt-5 rounded-xl border focus:outline-none w-full"
                 type="text"
                 name="email"
-                placeholder="Your email"
+                placeholder="Enter email"
               />
             </div>
             <PasswordInput />
-            <button className="Login-button rounded-xl text-white py-2">Login</button>
+            <button className="glow-effect rounded-full text-white ">Login</button>
+
           </form>
-          <div className="mt-3 text-xs flex justify-between items-center">
+
+          <div className="mt-5 text-xs flex justify-between items-center">
             <p>
-              
                 Forgot Password?
-              
             </p>
-            <button className="py-2 px-5 text-white Login-button rounded-xl">Register</button>
+            <button className="py-2 px-4 text-white Login-button rounded-xl">Register</button>
           </div>
         </div>
+        
+         
         
         <div className="md:w-1/2 flex img_hid items-center justify-center">
           <img className="w-full rounded-xl" src={login_img} alt='loginimage' />
