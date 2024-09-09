e6515f03654e8a4680c3e6b0215fc4c49cbe7227
DHUSHYU <135136525+DHUSHYANDAN@users.noreply.github.com>
2024-09-09 08:50:37 +0530

    Update navbar.js

--- voteing/src/components/navbar.js
+++ voteing/src/components/navbar.js
@@ -218,7 +218,6 @@ const Navvbars = ({ email }) => {
                                 alignItems: "center",
                                 justifyContent: "center",
                                 height: "45px",
-                                paddingBottom: "1px",
                                 width: "45px",
                                 borderRadius: "50%",
                                 fontSize: "2.25rem",
