339e5c9feee72f63925c0331fdc6649a26a6a97b
CHANDRUS-HUB <chandrumani72@gmail.com>
2024-09-15 01:42:26 +0530

    profile page designing

---
+++ voteing/src/pages/Profilepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Profile from "../components/profile";
+import Footer from "../components/footer";
+const Profilepage = () => {
+  return (
+    <div>
+      
+      <Navbar />
+      <Profile />
+      <Footer />
+    </div>
+  );
+};
+
+export default Profilepage;
