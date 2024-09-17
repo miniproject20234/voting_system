d61cf3274a05e7547018a8a1b80a62034c3c034d
DHUSHYU <135136525+DHUSHYANDAN@users.noreply.github.com>
2024-09-17 10:11:09 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -114,7 +114,7 @@ module.exports.forgot_password = async (req, res) => {
       to: email,
       subject: 'Reset Password Link',
       text: `Link is valid for 10min only. Click the link below:
-      http://localhost:3000/reset_password/${user._id}/${token}`
+      https://votevibe.vercel.app/reset_password/${user._id}/${token}`
     };
 
     try {
