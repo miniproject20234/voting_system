c9756dfa1bda51bd13a57916020a76edb473d8ca
CHANDRUS-HUB <chandrumani72@gmail.com>
2024-09-17 10:42:11 +0530

    profile page designing

--- voteing/src/components/Notfound.js
+++ voteing/src/components/Notfound.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import foundImage from '../assets/404-page.png';
 function Notfound() {
     
   return (
-    <div>Notfound</div>
+    <div>
+
+<img className=''  src={foundImage} alt='notfound'/>
+<a href="/"
+   class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent mt-4">
+   Back To Home
+</a>
+
+    </div>
   )
 }
 
