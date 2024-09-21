47a2162d315c908fd1732eddf6b9914824e0565f
CHANDRUS-HUB <chandrumani72@gmail.com>
2024-09-21 09:58:12 +0530

    notfound  page designing  not finish

--- voteing/src/components/Notfound.js
+++ voteing/src/components/Notfound.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import NotFound_img from "../assets/notFound.png";
-import {  NavLink } from 'react-router-dom';
+import NotFound_img from "../assets/error6.gif";
+import { NavLink } from 'react-router-dom';
 
 const Notfund = () => {
-  
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
-      <NavLink to='/'>  <img  className="mx-auto -mt-7" src={NotFound_img} alt="not found" /></NavLink>
-       
-        {/* <NavLink to='/' className='cursor-pointer'><button className="mt-4  bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
-          Back to Home
-        </button></NavLink> */}
-        
+    <div className="relative h-screen flex flex-col justify-center items-center bg-white">
+      {/* Blue circle on the top-left */}
+      <div className="absolute top-0 left-0 w-72 h-72 bg-blue-200 rounded-full opacity-70"></div>
+
+      {/* Blue circle on the bottom-right */}
+      <div className="absolute bottom-0 right-0 w-96 h-96 bg-blue-300 rounded-full opacity-70"></div>
+
+      {/* 404 content */}
+      <div className="z-10 text-center">
+        <NavLink to='/'>
+          <img className="mx-auto -mt-7" src={NotFound_img} alt="not found" />
+        </NavLink>
+        <div className="text-6xl font-bold text-blue-600 mt-4">404</div>
+        <p className="text-xl mt-4">Something went wrong.</p>
+        <p className="text-gray-500 mt-2">Sorry, we can't find the page you're looking for.</p>
+        <NavLink to='/'>
+          <button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-900 text-white rounded-lg">
+            Go Back
+          </button>
+        </NavLink>
       </div>
     </div>
   );
