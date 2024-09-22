d1e32ab27a69b238c7605b3a6abe81a26c37a025
DHUSHYU <smartworkdhushyu@gmail.com>
2024-09-23 01:49:35 +0530

    profile page and sign in sign up page

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,10 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-
-  origin:"https://votevibe.vercel.app/",
-  methods:"GET,POST,PUT,DELETE",
-  credentials:true
+  origin: 'https://votevibe.vercel.app/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 app.use(express.json());
