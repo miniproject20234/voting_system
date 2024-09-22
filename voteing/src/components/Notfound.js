c62a9d9bc8aa597881c35282d0a9b836b1c5e324
CHANDRUS-HUB <chandrumani72@gmail.com>
2024-09-22 21:10:39 +0530

    notfound  page designing  not finish

--- voteing/src/components/Notfound.js
+++ voteing/src/components/Notfound.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import NotFound_img from "../assets/error6.gif";
+import NotFound_img from "../assets/errors.png";
 import { NavLink } from 'react-router-dom';
 
 const Notfund = () => {
   return (
     <div className="relative h-screen flex flex-col justify-center items-center bg-white">
-      {/* Blue circle on the top-left */}
-      <div className="absolute top-0 left-0 w-72 h-72 bg-blue-200 rounded-full opacity-70"></div>
+      {/* Image with error text */}
+      <div className="z-6 text-center relative">
+        <img className="mx-auto mb-4" src={NotFound_img} alt="not found" />
 
-      {/* Blue circle on the bottom-right */}
-      <div className="absolute bottom-0 right-0 w-96 h-96 bg-blue-300 rounded-full opacity-70"></div>
-
-      {/* 404 content */}
-      <div className="z-10 text-center">
-        <NavLink to='/'>
-          <img className="mx-auto -mt-7" src={NotFound_img} alt="not found" />
-        </NavLink>
-        <div className="text-6xl font-bold text-blue-600 mt-4">404</div>
-        <p className="text-xl mt-4">Something went wrong.</p>
-        <p className="text-gray-500 mt-2">Sorry, we can't find the page you're looking for.</p>
-        <NavLink to='/'>
-          <button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-900 text-white rounded-lg">
-            Go Back
-          </button>
-        </NavLink>
+        {/* Absolutely positioned "Go Back" button */}
+        <div className="absolute" style={{ top: '84%', left: '50%', transform: 'translate(-50%, 0)' }}>
+          <NavLink to='/'>
+            <button className="px-4 py-2 bg-blue-600 hover:bg-blue-900 text-white rounded-lg">
+              Go Back
+            </button>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
